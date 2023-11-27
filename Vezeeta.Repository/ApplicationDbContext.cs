2c10c14071153a12aec0a68aaa72257f468ff9ee
amr mahmoud <amrmahmoudfcb11@gmail.com>
2023-11-27 22:29:08 +0200

    Init Project

---
+++ Vezeeta.Repository/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Castle.Core.Resource;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Vezeeta.Core.Domain.User;
+
+namespace Vezeeta.Repository
+{
+    public interface IApplicationDbContext
+    {
+        //Task<int> SaveChangesAsync();
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<User, Role, Guid>, IApplicationDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
